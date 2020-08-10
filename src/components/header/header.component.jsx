c3af21ed216047e58cc36526ff592dde2d2a3aba
Fabian-MG <mirandafabian1510@gmail.com>
2020-08-10 10:26:22 -0500

    Changing header background on scroll

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,11 +7,42 @@ import "./header.styles.scss";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const [isTop, setIsTop] = React.useState(true);
+  const [windowHeight, setWindowHeight] = React.useState(0);
+
+  React.useEffect(() => {
+    const onScroll = () => {
+      const navigationHeight = 5 * 8;
+      if (window.scrollY + navigationHeight < windowHeight) {
+        setIsTop(true);
+      } else {
+        setIsTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [windowHeight]);
+
+  React.useEffect(() => {
+    const onResize = () => {
+      setWindowHeight(window.innerHeight);
+    };
+
+    setWindowHeight(window.innerHeight);
+
+    window.addEventListener("resize", onResize);
+
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
 
   return (
-    <div className="Header">
+    <div className={`Header ${isTop ? "" : "active"}`}>
       <div className="logo-container">
-        <span className='title'></span>
+        <div className='logo'/>
+        <span className="title">Hola</span>
+
       </div>
       <div className="menu-container">
         <FullMenu toggle={toggle} />
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,10 +3,12 @@ import Slider from "../../components/slider/slider.component";
 
 const HomePage = () => {
   return (
-      <div className='HomePage'>
-        <Slider />
-      </div>
-  )
+    <div className="HomePage">
+      <Slider />
+      <div style={{ height: "800px", width: "100vw", background: "white" }}>
+        </div>
+    </div>
+  );
 };
 
 export default HomePage;
