d815b788fb672a69c20118894a857403fca0ff6a
Fabian-MG <mirandafabian1510@gmail.com>
2020-08-18 21:06:28 -0500

    Changing remote

--- src/components/collection-preview/collection-preview.components.jsx
+++ src/components/collection-preview/collection-preview.components.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-import "./devotional-preview.styles.scss";
+import "./collection-preview.styles.scss";
 import { Link } from "react-router-dom";
+import CustomLinkButton from "../custom-link-button/custom-link-button.component";
 
 const CollectionPreview = ({ title, imageUrl, inverted }) => {
   return (
-    <div className={`collection-preview-container ${inverted ? 'inverted' : ''}`}>
-      <h2 className="cp-title">{title}</h2>
+    <div
+      className={`collection-preview-container ${inverted ? "inverted" : ""}`}
+    >
       <div className="content-container">
         <div className="image-container">
           <div
@@ -15,13 +17,14 @@ const CollectionPreview = ({ title, imageUrl, inverted }) => {
           />
         </div>
         <div className="content">
+          <h2 className="cp-title">{title}</h2>
           <p className="cp-description">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
-          <Link className="cp-link">Go To</Link>
+          <CustomLinkButton title="Saber más" path="" />
         </div>
       </div>
     </div>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -7,7 +7,7 @@ import MinistriesPreview from "../../components/ministries-preview/ministries-pr
 
 import "./homepage.styles.scss";
 import Footer from "../../components/footer/footer.component";
-import CollectionPreview from "../../components/devotional-preview/devotional-preview.components";
+import CollectionPreview from "../../components/collection-preview/collection-preview.components";
 
 const HomePage = () => {
   return (
@@ -17,20 +17,11 @@ const HomePage = () => {
       <h1 className="section-title">Ministerios</h1>
       <FloatingCard />
       <MinistriesPreview />
-      <div className="study-content-container">
-        <CollectionPreview
-          title="Clases"
-          imageUrl="https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        />
-        <CollectionPreview
-          title="Devocionales"
-          imageUrl="https://images.pexels.com/photos/159679/bible-job-reading-christianity-159679.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        />
-        <CollectionPreview
-          title="Eventos"
-          imageUrl="https://images.pexels.com/photos/273153/pexels-photo-273153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        />
-      </div>
+      <CollectionPreview
+        title="Clases"
+        imageUrl="https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+      />
+
       <Footer />
     </div>
   );
